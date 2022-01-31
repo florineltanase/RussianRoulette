3fff29083caa02587404f45512caf97f0a91e34a
Florinel Tanase <62656980+florineltanase@users.noreply.github.com>
2022-01-31 15:34:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-function pressTheButton()
-{
-   let x=[1,2,3,4,5,6];
-   let tries=0;
-   let www=getRandomInt(1,6);
-   console.log(www)
-   const texting=document.querySelector("mort")
-   
-   const button = document. querySelector('button')
-    
-   let number=Math.floor(Math.random()*x.length)
-    
-   console.log(number)
+let tries = 0;
+const button1 = document.querySelector("btn");
 
- 
-    if(number===www)
-    {
-       document.body.style.backgroundImage="url('hero_nuclear_blast.jpg')";
+function Verificare() {
+  let check = 0;
+
+  if (number === www) {
+    check = 1;
+  }
+
+  return check;
+}
+
+function pressTheButton() {
+  x = [1, 2, 3, 4, 5, 6];
+  www = getRandomInt(1, 6);
+  number = Math.floor(Math.random() * x.length);
+
+  console.log("nr din x" + number);
+  console.log("nr radon" + www);
+    if (Verificare() == 0 && tries>=7) {
+      document.getElementById("demo").innerHTML = "Hai ca ai scapat patroane";
+    } else if (Verificare() == 1 && tries<=7) {
+      document.getElementById("demo").innerHTML = "GAME OVER HANG OVER";
+      document.body.style.backgroundColor = "#ff0000";
       
     }
 
-
+    console.log("tries" + tries);
+    tries++;
+  
 }
 
-function ResetAll(){
-    document.body.style.backgroundImage="url('parc.pg.jpg')"
-
+function ResetAll() {
+  document.body.style.backgroundColor = "#00ff00";
+  tries = 0;
+  document.getElementById("demo").innerHTML = " ";
+  console.clear();
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
